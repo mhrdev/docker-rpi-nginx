6cd7b098f8cf561d33a929a2f54fba1769c2b400
Marius Rumpf <marius@mariusrumpf.de>
2015-12-27 23:15:43 +0100

    Compile with -fPIE and -pie to fix segfault erros when using proxy_pass

--- build/extractdeb.sh
+++ build/extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-containerID=$(docker run --detach mariusrumpf/buildnginx)
+containerID=$(docker run -d mariusrumpf/buildnginx)
 docker cp $containerID:/root/build/nginx_1.9.9-1~jessie_armhf.deb .
 sleep 1
 docker rm $containerID
