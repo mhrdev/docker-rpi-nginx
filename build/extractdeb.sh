e0bf7b22833b4408be38c5a2218bdaddb00a74a1
Marek Hruzik <mhrjob@gmail.com>
2016-12-01 18:44:05 +0000

    Updated nginx version; fixed key issues

--- build/extractdeb.sh
+++ build/extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
-
+VERSION=1.11.6-1~jessie
 containerID=$(docker run -d mariusrumpf/buildnginx)
-docker cp $containerID:/root/build/nginx_1.9.9-1~jessie_armhf.deb .
+docker cp $containerID:/root/build/nginx_${VERSION}_armhf.deb .
 sleep 1
 docker rm $containerID
